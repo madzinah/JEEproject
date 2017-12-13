b7ddde26d835186c6a7f695617e0153caf2fecf8
Geoffrey <geoffrey.spaur@gmail.com>
2017-12-13 12:02:30 +0100

    try_db

--- src/main/java/jeeproject/jeeproject/Controllers/MyFirstLittleController.java
+++ src/main/java/jeeproject/jeeproject/Controllers/MyFirstLittleController.java
@@ -1,5 +1,8 @@
 package jeeproject.jeeproject.Controllers;
 
+import jeeproject.jeeproject.Entity.User;
+import jeeproject.jeeproject.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +13,17 @@ import java.util.Map;
 @Controller
 public class MyFirstLittleController {
 
+    @Autowired
+    private UserRepository userRepository;
+
     @RequestMapping("/hello/{name}")
     public String helloWorld(@PathVariable String name, Map<String, Object> model) {
+
+        User n = new User();
+        n.setPassword(name);
+        n.setEmail(name);
+        userRepository.save(n);
+
         model.put("message", "Coucou " + name + " !!!");
         return "hello";
     }
