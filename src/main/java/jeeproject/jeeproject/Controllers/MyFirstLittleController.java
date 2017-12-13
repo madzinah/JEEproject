3184349c0e08ba2329c62ceb1d537ee5bd2f3d17
Geoffrey <geoffrey.spaur@gmail.com>
2017-12-13 11:24:04 +0100

    Request with parameters

--- src/main/java/jeeproject/jeeproject/Controllers/MyFirstLittleController.java
+++ src/main/java/jeeproject/jeeproject/Controllers/MyFirstLittleController.java
@@ -1,13 +1,18 @@
 package jeeproject.jeeproject.Controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
 
 @Controller
 public class MyFirstLittleController {
 
-    @RequestMapping("/hello")
-    public String helloWorld() {
+    @RequestMapping("/hello/{name}")
+    public String helloWorld(@PathVariable String name, Map<String, Object> model) {
+        model.put("message", "Coucou " + name + " !!!");
         return "hello";
     }
 }
