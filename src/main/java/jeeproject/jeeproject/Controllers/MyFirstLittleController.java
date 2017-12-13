e228c1cd4e1da650e3ad7daff350720a1591666f
Geoffrey <geoffrey.spaur@gmail.com>
2017-12-13 10:54:44 +0100

    Config stratup

---
+++ src/main/java/jeeproject/jeeproject/Controllers/MyFirstLittleController.java
@@ -0,0 +1,13 @@
+package jeeproject.jeeproject.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MyFirstLittleController {
+
+    @RequestMapping("/hello")
+    public String helloWorld() {
+        return "Hello world !!";
+    }
+}
