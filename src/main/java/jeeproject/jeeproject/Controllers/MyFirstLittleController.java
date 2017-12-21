1ee66b210e837f101cf0689585a20fd5307fffcb
Geoffrey <geoffrey.spaur@gmail.com>
2017-12-21 09:53:42 +0100

    Fix bd & premier test de session

--- src/main/java/jeeproject/jeeproject/Controllers/MyFirstLittleController.java
+++ src/main/java/jeeproject/jeeproject/Controllers/MyFirstLittleController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpSession;
 import java.util.Map;
 
 @Controller
@@ -23,8 +24,21 @@ public class MyFirstLittleController {
         n.setPassword(name);
         n.setEmail(name);
         userRepository.save(n);
-
         model.put("message", "Coucou " + name + " !!!");
         return "hello";
+    }
+
+    @RequestMapping("/session/add")
+    public String addSession(Map<String, Object> model, HttpSession httpSession) {
+        Integer o = (Integer)httpSession.getAttribute("testSession");
+        httpSession.setAttribute("testSession", o == null ? 0 : ++o);
+        model.put("testSession", httpSession.getAttribute("testSession"));
+        return "testSession";
+    }
+
+    @RequestMapping("/session/get")
+    public String getSession(Map<String, Object> model, HttpSession httpSession) {
+        model.put("testSession", httpSession.getAttribute("testSession"));
+        return "testSession";
     }
 }
