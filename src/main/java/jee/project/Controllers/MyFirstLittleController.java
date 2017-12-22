d66a861d2df065121980830291d267de87620fba
CorentinLeGuen <leguen.corentin@mail.com>
2017-12-22 10:15:28 +0100

    Rewrite packages

--- src/main/java/jee/project/Controllers/MyFirstLittleController.java
+++ src/main/java/jee/project/Controllers/MyFirstLittleController.java
@@ -1,4 +1,4 @@
-package jeeproject.jeeproject.Controllers;
+package jee.project.Controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
