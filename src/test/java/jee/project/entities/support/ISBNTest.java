0f198c38b7dbcd842e83787f6cf06a30e8b96985
CorentinLeGuen <leguen.corentin@mail.com>
2017-12-26 16:35:26 +0100

    Adding first Test : ISBNTest and adding JUnit to the project

---
+++ src/test/java/jee/project/entities/support/ISBNTest.java
@@ -0,0 +1,22 @@
+package jee.project.entities.support;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ISBNTest {
+
+    @Test
+    public void isISBN() {
+        assertTrue(ISBN.isISBN("2-7201-0011-0"));
+        assertTrue(ISBN.isISBN("0-9752298-0-X"));
+        assertTrue(ISBN.isISBN("99921-58-10-7"));
+        assertTrue(ISBN.isISBN("9971-5-0210-0"));
+        assertTrue(ISBN.isISBN("0-85131-041-9"));
+
+        assertFalse(ISBN.isISBN(null));
+        assertFalse(ISBN.isISBN("978-1-2345-6789-7"));
+        assertFalse(ISBN.isISBN("0-5961-52068"));
+        assertFalse(ISBN.isISBN(""));
+    }
+}
