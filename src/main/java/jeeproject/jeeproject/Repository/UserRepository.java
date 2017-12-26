5d6a6a01f66d0efbe1a9a4952b464d8f88c1b6eb
CorentinLeGuen <leguen.corentin@mail.com>
2017-12-26 16:34:44 +0100

    Deleting useless classes : MyFirstLittleController and User/UserRepo

--- src/main/java/jeeproject/jeeproject/Repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package jeeproject.jeeproject.Repository;
-
-import jeeproject.jeeproject.Entity.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Long> {
-
-}
-
