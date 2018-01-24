944e61a51096132c5c9b366f747e78fd9462ebc1
Unknown <geoffrey.spaur@gmail.com>
2018-01-24 19:15:20 +0100

    First angular ajax call to backend api

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { AppForm } from './tuto/form';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppForm
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent,AppForm]
 })
 export class AppModule { }
--- src/main/java/jeeproject/jeeproject/Controllers/MyFirstLittleController.java
+++ src/main/java/jeeproject/jeeproject/Controllers/MyFirstLittleController.java
@@ -1,44 +1,37 @@
 package jeeproject.jeeproject.Controllers;
 
-import jeeproject.jeeproject.Entity.User;
 import jeeproject.jeeproject.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpSession;
-import java.util.Map;
-
-@Controller
+@RestController
 public class MyFirstLittleController {
 
     @Autowired
     private UserRepository userRepository;
 
-    @RequestMapping("/hello/{name}")
-    public String helloWorld(@PathVariable String name, Map<String, Object> model) {
 
-        User n = new User();
-        n.setPassword(name);
-        n.setEmail(name);
-        userRepository.save(n);
-        model.put("message", "Coucou " + name + " !!!");
-        return "hello";
+
+    @RequestMapping("/api/hello/{name}")
+    public Hello helloApi(@PathVariable String name) {
+        return new Hello(name);
     }
 
-    @RequestMapping("/session/add")
-    public String addSession(Map<String, Object> model, HttpSession httpSession) {
-        Integer o = (Integer)httpSession.getAttribute("testSession");
-        httpSession.setAttribute("testSession", o == null ? 0 : ++o);
-        model.put("testSession", httpSession.getAttribute("testSession"));
-        return "testSession";
+    @RequestMapping("/test35")
+    public String test() {
+        return "test";
     }
 
-    @RequestMapping("/session/get")
-    public String getSession(Map<String, Object> model, HttpSession httpSession) {
-        model.put("testSession", httpSession.getAttribute("testSession"));
-        return "testSession";
+    public class Hello {
+        public Hello(String name) {
+            this.name = name;
+        }
+        private String name;
+
+        public String getName() {
+            return name;
+        }
     }
 }
